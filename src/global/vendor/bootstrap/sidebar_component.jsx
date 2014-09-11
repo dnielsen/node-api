5a162edb54d89f6ae1a0f755e1ea57257ea0d3e6
Shripad K <assortmentofsorts@gmail.com>
2014-09-11 19:36:08 +0530

    scrollTop fix

--- src/global/vendor/bootstrap/sidebar_component.jsx
+++ src/global/vendor/bootstrap/sidebar_component.jsx
@@ -55,7 +55,7 @@ var SidebarControlBtn = React.createClass({
   componentDidMount: function() {
     ReactBootstrap.Dispatcher.on('sidebar:controlbtn', this.handleState);
     setTimeout(function() {
-      $('body, #body').scrollTop(0);
+      $('html, body, #body').scrollTop(0);
     }, 15);
   },
   componentWillUnmount: function() {
